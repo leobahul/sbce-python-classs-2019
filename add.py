2f7eb69b430cae80eece95e39c46ba4412f3c87e
leobahul <b.bahul007@gmail.com>
2019-02-23 12:48:54 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,7 @@
+def display():
+    print(__name__)
+
+
+def addition(x,y):
+    z=x+y
+    return z
---
+++ calc.py
@@ -0,0 +1,10 @@
+import add
+
+
+# num1=int(input("enter first num"))
+# num2=int(input("enter second num"))
+# result=add.addition(num1,num2)
+# print(result)
+
+print(__name__)
+add.display()
